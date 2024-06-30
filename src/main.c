8511112f16bf0198041c7bf2b1fb58d1e21a7b9b
Andrey Arthur <andrey_arthur1@outlook.com>
2024-06-30 00:01:03 -0300

    implement a better command line integration

--- src/main.c
+++ src/main.c
@@ -1,6 +1,38 @@
 #include "include/evaluator.h"
 #include "include/parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *get_input(int argc, char **argv) {
+    int length = argc - 1;
+    char **arguments = argv + 1;
+
+    char *help_message =
+        "Usage: calc '2 + 2'\nEvaluate math operations to standard output.\n\n "
+        "   -h, --help display this message\n\nIf no argument is given, it "
+        "opens a REPL.\n";
+
+    if (length >= 2) {
+        printf("%s", help_message);
+        exit(1);
+        return NULL;
+    }
+
+    if (length == 0) {
+        return NULL;
+    }
+
+    char *argument = arguments[0];
+
+    if (strcmp(argument, "-h") == 0 || strcmp(argument, "--help") == 0) {
+        printf("%s", help_message);
+        exit(0);
+        return NULL;
+    }
+
+    return arguments[0];
+}
 
 double get_result(const char *content) {
     Lexer *lexer = lexer_init(content);
@@ -32,12 +64,13 @@ void repl() {
 }
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        repl();
-        return 0;
-    }
+    char *input = get_input(argc, argv);
 
-    printf("%lg\n", get_result(argv[1]));
+    if (input == NULL) {
+        repl();
+    } else {
+        printf("%lg\n", get_result(input));
+    }
 
     return 0;
 }
