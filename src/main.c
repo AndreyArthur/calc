d0c7bca211fe3fd39171cca0fd6033ce4cb447d5
Andrey Arthur <andrey_arthur1@outlook.com>
2024-06-29 23:07:22 -0300

    make a repl and get argv

--- src/main.c
+++ src/main.c
@@ -1,12 +1,43 @@
-#include "include/token.h"
-#include <assert.h>
+#include "include/evaluator.h"
+#include "include/parser.h"
 #include <stdio.h>
 
-int main() {
-    Token *token = token_init(TOKEN_NUMBER, "24");
-    printf("%d %s\n", token->type, token->value);
-    token_free(&token);
+double get_result(const char *content) {
+    Lexer *lexer = lexer_init(content);
+    Token **tokens = lexer_lex(lexer);
+    lexer_free(&lexer);
 
-    assert(token == NULL);
+    Parser *parser = parser_init(tokens);
+    Ast *ast = parser_parse(parser);
+    parser_free(&parser);
+
+    double result = eval(ast);
+    ast_free(&ast);
+
+    return result;
+}
+
+void repl() {
+    printf("Welcome to the calc REPL! Type in math expressions.\n");
+
+    for (;;) {
+        printf(">> ");
+
+        char buffer[1024];
+
+        scanf(" %1023[^\n]", buffer);
+
+        printf("%lg\n", get_result(buffer));
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        repl();
+        return 0;
+    }
+
+    printf("%lg\n", get_result(argv[1]));
+
     return 0;
 }
